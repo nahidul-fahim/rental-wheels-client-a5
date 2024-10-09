63812bc4e1cca79b0ef7d9aef42fb551b6733b3a
nahidul-fahim <nahidul7562@gmail.com>
2024-10-09 20:38:01 +0600

    added breadcrumb to admin dashboard

--- src/pages/adminDashboard/AdminDashboard.tsx
+++ src/pages/adminDashboard/AdminDashboard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { IoLogOutOutline } from "react-icons/io5";
+import { IoChevronForward, IoHome, IoLogOutOutline } from "react-icons/io5";
 import DashboardOverview from './DashboardOverview';
 import ManageCars from './ManageCars';
 import ManageBookings from './ManageBookings';
@@ -16,7 +16,24 @@ const AdminDashboard: React.FC = () => {
     return (
         <div className="container mx-auto px-4 sm:px-6 py-6 min-h-screen">
             <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-6 sm:mb-8 gap-4 sm:gap-0">
-                <h1 className="text-3xl sm:text-4xl font-bold text-primary">Admin Dashboard</h1>
+                <div className='space-y-3'>
+                    <h1 className="text-3xl sm:text-4xl font-bold text-primary">Admin Dashboard</h1>
+                    {/* Breadcrumb Navigation */}
+                    <nav className="flex items-center text-gray-500 mb-8" aria-label="Breadcrumb">
+                        <ol className="flex items-center space-x-2">
+                            <li>
+                                <a href="/" className="hover:text-primary flex items-center">
+                                    <IoHome className="flex-shrink-0 h-5 w-5" />
+                                    <span className="sr-only">Home</span>
+                                </a>
+                            </li>
+                            <li className="flex items-center">
+                                <IoChevronForward className="flex-shrink-0 h-5 w-5" />
+                                <span className="ml-2 text-gray-700 font-medium">Admin Dashboard</span>
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
                 <Button onClick={() => handleLogout()}>
                     <IoLogOutOutline className="mr-2 text-xl" /> Logout
                 </Button>
